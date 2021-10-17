eeb1c9d8717418c436af345b6ae5494c1246e607
HoangTranUnion <henry11102000@gmail.com>
2021-10-17 11:41:53 +0700

    added error message when input is not YouTube link

--- src/ui/subclasses/MainUI.py
+++ src/ui/subclasses/MainUI.py
@@ -1,4 +1,6 @@
+import re
 from PyQt5 import QtWidgets, QtGui, QtCore
+
 from src.ui.generated import MainScreen
 from src.main.storage import AppStorage
 from src.main.load_url import LoadURL
@@ -13,10 +15,20 @@ class MainMenu(QtWidgets.QMainWindow, MainScreen.Ui_MainWindow):
         self.LoadButton.clicked.connect(lambda: self.url_loading(self.linkInput.text()))
         self._cur_titles_shown = []
 
+    @staticmethod
+    def is_valid_youtube_url(given_url):
+        return bool(re.search(r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$', given_url))
+
     def url_loading(self, input_url):
-        self.load_worker = LoadWorker(input_url, self.storage)
-        self.load_worker.finished.connect(self._update_view)
-        self.load_worker.start()
+        if not self.is_valid_youtube_url(input_url): # bilibili support will come later, please wait for it Rem :p
+            self.error_dialog = QtWidgets.QErrorMessage()
+            self.error_dialog.setWindowTitle('Warning')
+            self.error_dialog.showMessage('Please enter a valid YouTube video/playlist link.')
+            self.error_dialog.exec_()
+        else:
+            self.load_worker = LoadWorker(input_url, self.storage)
+            self.load_worker.finished.connect(self._update_view)
+            self.load_worker.start()
 
     def _update_view(self):
         self.listWidget.clear()
